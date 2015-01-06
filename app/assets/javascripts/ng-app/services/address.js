3a4f90db2ce8a236ab4f6783610e99fa22503bdc
rgpass <rgpass@gmail.com>
2015-01-06 15:04:10 -0500

    Add second address field

--- app/assets/javascripts/ng-app/services/address.js
+++ app/assets/javascripts/ng-app/services/address.js
@@ -4,13 +4,13 @@ angular.module('myApp')
   var that = this;
   var resultsUrl = '/results';
 
-  this.getResults = function(address) {
-    params = { address: address };
+  this.getResults = function(addresses) {
+    params = { addresses: JSON.stringify(addresses) };
     return $http.get(resultsUrl + '.json', { params: params }).success(function(data) {
       that.error    = null;
       that.results  = data;
     }).error(function() {
-      that.error    = 'Something went wrong. Please confirm the address is correct and that you have an internet connection.';
+      that.error    = 'Something went wrong. Please check the address(es) and your internet connection.';
       that.results  = null;
     });
   };
