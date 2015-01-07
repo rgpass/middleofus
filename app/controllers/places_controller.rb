cc75231be4adb4ff4c3b9adfc552c0b4ac2c5c00
rgpass <rgpass@gmail.com>
2015-01-07 14:29:17 -0500

    JSON boolean for is_open

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
   def results
-    addresses = JSON.parse(params[:addresses]) || ["650 North Avenue NE Atlanta GA"]
+    example = ["650 North Avenue NE Atlanta GA"]
+    addresses = params[:addresses] ? JSON.parse(params[:addresses]) : example
     average_coords = Place.average_coords(addresses)
     @places = Place.wifi_near(average_coords)
   end
