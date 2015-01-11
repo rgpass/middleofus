985c27fbdf7920e45c35210993492bf12cc6ca87
rgpass <rgpass@gmail.com>
2015-01-11 13:13:28 -0500

    Complete address validation

--- app/assets/javascripts/ng-app/controllers/address.js
+++ app/assets/javascripts/ng-app/controllers/address.js
@@ -1,6 +1,36 @@
 angular.module('myApp')
 .controller('AddressCtrl', ['$scope', 'addressesService', function ($scope, addressesService) {
 
+  $scope.isFirstValid = true;
+  $scope.isSecondValid = true;
+
+  $scope.$watch('formData.addressOne', setFirstValidations);
+  $scope.$watch('formData.addressTwo', setSecondValidations);
+
+  function setFirstValidations(newValue, oldValue) {
+    if (newValue) {
+      addressesService.isValidAddress($scope.formData.addressOne).success(function(data) {
+        $scope.isFirstValid = data.is_valid;
+        $scope.isFirstEmpty = false;
+      });
+    } else {
+      $scope.isFirstEmpty = true;
+      $scope.isFirstValid = true;
+    }
+  }
+
+  function setSecondValidations(newValue, oldValue) {
+    if (newValue) {
+      addressesService.isValidAddress($scope.formData.addressTwo).success(function(data) {
+        $scope.isSecondValid = data.is_valid;
+        $scope.isSecondEmpty = false;
+      });
+    } else {
+      $scope.isSecondEmpty = true;
+      $scope.isSecondValid = true;
+    }
+  };
+
   $scope.submitAddresses = function() {
     var addresses = [$scope.formData.addressOne, $scope.formData.addressTwo];
     addressesService.getResults(addresses).success(setVariables).error(setVariables);
