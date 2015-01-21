ac033e322a141ff7775974824ca9075735dcfd98
rgpass <rgpass@gmail.com>
2015-01-21 14:15:55 -0500

    PA

--- public/assets/application-1d41705da6a24696d1eb00c2e304bb67.js
+++ public/assets/application-1d41705da6a24696d1eb00c2e304bb67.js
@@ -61758,7 +61758,7 @@ angular.module('myApp')
 
   function scrollToResults() {
     $('body').animate({
-        scrollTop: $("#results").offset().top
+      scrollTop: $("#results").offset().top
     }, 500);
   }
 
@@ -61930,7 +61930,7 @@ angular.module('myApp')
 
 
 
-$(document).click(function (event) {
+$(document).on('click tap', function (event) {
   var clickover = $(event.target);
   var $navbar = $(".navbar-collapse");               
   var _opened = $navbar.hasClass("in");
