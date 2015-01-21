19fc2fcacedf364767c630fe69ef7ac0decaf24b
rgpass <rgpass@gmail.com>
2015-01-21 11:43:47 -0500

    PA

--- public/assets/application-ff44cb0e7f16cd22df20948e973fbdde.js
+++ public/assets/application-ff44cb0e7f16cd22df20948e973fbdde.js
@@ -61919,4 +61919,12 @@ angular.module('myApp')
 
 
 
-;
+
+$(document).click(function (event) {
+  var clickover = $(event.target);
+  var $navbar = $(".navbar-collapse");               
+  var _opened = $navbar.hasClass("in");
+  if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+    $navbar.collapse('hide');
+  }
+});
