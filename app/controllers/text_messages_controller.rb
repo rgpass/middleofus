fc6d1fedb29f08df5274b18e054114c12227a2f6
Bryan Lackey <bryan.lackey@gmail.com>
2015-03-30 22:49:00 -0400

    separated concerns of twilio configuration and text messaging in text_message model

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,9 +1,7 @@
 class TextMessagesController < ApplicationController
 
   def send_location_info
-    params[:address]      ||= "123 Fake Street"
-    params[:phone_number] ||= "4042180454"
-    params[:place]        ||= "Taco Bell"
+    params[:address, :phone_number, :place]            
     TextMessage.send_location_info(params)
 
     respond_to do |format|
--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -1,26 +1,25 @@
 class TextMessage
 
-  def self.send_location_info(args)
-    args[:address]      ||= "123 Fake Street"
-    args[:phone_number] ||= "8042918214"
-    args[:place]        ||= "Taco Bell"
-    phone_number  = args[:phone_number]
-    place         = args[:place]
-    address       = args[:address]
-
+  def self.configure_twilio
     account_sid = 'ACd2ddae5650ca06720b7a7ba52f6a04d4'
     auth_token = '1d2ced219c26754a5bd66f297aa45945'
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    Twilio::REST::Client.new account_sid, auth_token
+  end
 
-    signature   = "- Courtesy of MiddleOf.Us"
+  def self.send_message(message, phone_number)
+    configure_twilio.messages.create(
+      body: message,
+      to:   phone_number,
+      from: "+17702855442"
+    )    
+  end
 
-    message = "#{place} \n #{address} \n #{signature}"
-
-    @client.account.messages.create(
-      :body => message,
-      :to   => phone_number,
-      :from => "+17702855442"
-    )
+  def self.send_location_message(params) 
+    phone_number  = args[:phone_number] ||= '123 Fake Street'
+    place         = args[:place]        ||= '8042918214'
+    address       = args[:address]      ||= 'Taco Bell'
+    message       = "#{place} \n #{address} \n - Courtesy of MiddleOf.Us"       
+    send_message(message, phone_number)
   end
 
 end
